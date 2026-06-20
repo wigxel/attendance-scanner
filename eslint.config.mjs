a2743fae10554d7ed28c6caa88438ee6470d9354
Joseph Owonvwon <joseph.owonwo@gmail.com>
2026-06-20 01:04:05 +0100

    chore: moved from next lint to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,17 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
   },
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
