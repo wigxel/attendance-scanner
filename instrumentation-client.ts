3da9bbbf1eb4d20c517cc5b460a07ec78c03de35
Joseph Owonvwon <joseph.owonwo@gmail.com>
2026-07-02 14:18:31 +0100

    chore: updated posthog config

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,5 +3,6 @@ import { safeStr } from "./lib/data.helpers";
 
 posthog.init(safeStr(process.env.NEXT_PUBLIC_POSTHOG_KEY), {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
   defaults: "2025-11-30",
 });
